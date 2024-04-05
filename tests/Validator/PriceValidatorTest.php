2bdc72861d90e98753c1e61a54a0e69a221b1e7d
André Oehmicke <oehmicke@gmail.com>
2024-04-05 13:41:54 +0200

    Add PriceValidatorTest for product price validation
    Integrates a new PriceValidatorTest file which introduces a PriceValidator for enhanced validation of product prices. This modification ensures consistent product price format as "0.00 EUR" and checks for invalid cases such as text input, missing currency, and missing fraction. Also, it ensures validation for null values and valid prices.

---
+++ tests/Validator/PriceValidatorTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Tests\Validator;
+
+use App\Validator\Price;
+use App\Validator\PriceValidator;
+use Symfony\Component\Validator\Constraints\Currency;
+use Symfony\Component\Validator\ConstraintValidatorInterface;
+use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
+
+class PriceValidatorTest extends ConstraintValidatorTestCase
+{
+    protected function createValidator(): ConstraintValidatorInterface
+    {
+        return new PriceValidator();
+    }
+
+    public function testNullIsValid(): void
+    {
+        $this->validator->validate(null, new Price());
+
+        $this->assertNoViolation();
+    }
+
+    public function testValidPrice(): void
+    {
+        $this->validator->validate('22.22 EUR', new Price());
+
+        $this->assertNoViolation();
+    }
+
+    /**
+     * @dataProvider provideInvalidConstraints
+     */
+    public function testInvalidPrices(string $value, Price|Currency $constraint): void
+    {
+        $this->validator->validate($value, $constraint);
+
+        $this->buildViolation('some error')
+            ->setParameter('{{ string }}', $value)
+            ->assertRaised();
+    }
+
+    public function provideInvalidConstraints(): \Generator
+    {
+        yield ['some text' ,new Price(message: 'some error')];
+        yield ['.22 EUR' ,new Price(message: 'some error')]; //missing leading number
+        yield ['2.22' ,new Price(message: 'some error')]; //missing currency
+        yield ['2.0 EUR', new Price(message: 'some error')]; //missing fraction
+    }
+
+}
