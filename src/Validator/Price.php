f32f8ecf6fbd83dc11996f12bfc1de8f512c31d4
André Oehmicke <oehmicke@gmail.com>
2026-03-30 11:47:23 +0200

    Make PriceValidator constructor parameters nullable for improved flexibility

--- src/Validator/Price.php
+++ src/Validator/Price.php
@@ -10,7 +10,7 @@ class Price extends Constraint
     public string $message = 'The string "{{ string }}" should be in the format of "0.00 EUR"';
     public string $mode = 'strict';
 
-    public function __construct(string $mode = null, string $message = null, array $groups = null, $payload = null)
+    public function __construct(?string $mode = null, ?string $message = null, array|null $groups = null, mixed $payload = null)
     {
         parent::__construct([], $groups, $payload);
 
