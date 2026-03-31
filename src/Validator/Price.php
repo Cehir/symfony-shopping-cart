8b99ad3f94d257d7b8e0936739bd3498e82c43f6
André Oehmicke <oehmicke@gmail.com>
2026-03-31 18:47:12 +0200

    Resolve deprecated class constrain in Price

--- src/Validator/Price.php
+++ src/Validator/Price.php
@@ -4,18 +4,16 @@ namespace App\Validator;
 
 use Symfony\Component\Validator\Constraint;
 
-#[\Attribute]
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_CLASS)]
 class Price extends Constraint
 {
-    public string $message = 'The string "{{ string }}" should be in the format of "0.00 EUR"';
-    public string $mode = 'strict';
-
-    public function __construct(?string $mode = null, ?string $message = null, array|null $groups = null, mixed $payload = null)
+    public function __construct(
+        public ?string $mode = 'strict',
+        public ?string $message = 'The string "{{ string }}" should be in the format of "0.00 EUR"',
+        array|null $groups = null,
+        mixed $payload = null)
     {
-        parent::__construct([], $groups, $payload);
-
-        $this->mode = $mode ?? $this->mode;
-        $this->message = $message ?? $this->message;
+        parent::__construct(groups: $groups, payload: $payload);
     }
 
 }
